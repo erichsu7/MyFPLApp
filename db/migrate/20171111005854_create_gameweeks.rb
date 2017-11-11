57bb1397ac3ad81cbe1cdf337ca3a708b1baa5d4
Eric Hsu <erichsu7@gmail.com>
2017-11-10 17:04:27 -0800

    Create Gameweek.

---
+++ db/migrate/20171111005854_create_gameweeks.rb
@@ -0,0 +1,14 @@
+class CreateGameweeks < ActiveRecord::Migration
+  def change
+    create_table :gameweeks do |t|
+      t.string :name, null: false
+      t.integer :number, null: false
+      t.datetime :deadline_time, null: false
+      t.timestamp :deadline_time_epoch, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :gameweeks, :number, unique: true
+  end
+end
