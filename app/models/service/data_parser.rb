94a34db899da2b4beb5a68456b0c20c3c135ef12
Eric Hsu <erichsu7@gmail.com>
2017-11-19 22:11:26 -0800

    Create EventsParser.

--- app/models/service/data_parser.rb
+++ app/models/service/data_parser.rb
@@ -5,7 +5,7 @@ class Service::DataParser
   end
 
   def call
-    parse_gameweeks(@data["events"])
+    Service::DataParser::EventsParser.new(@data["events"]).call
     Service::DataParser::TeamsParser.new(@data["teams"]).call
     self
   end
@@ -13,18 +13,5 @@ class Service::DataParser
   private
 
   attr_reader :data
-
-  def parse_gameweeks(events)
-    events.each do |event|
-      gameweek = Gameweek.find_or_initialize_by(
-        name: event["name"],
-        number: event["id"]
-      )
-      gameweek.new_record? && gameweek.update(
-        deadline_time: event["deadline_time"],
-        deadline_time_epoch: event["deadline_time_epoch"]
-      )
-    end
-  end
 
 end
---
+++ app/models/service/data_parser/events_parser.rb
@@ -0,0 +1,20 @@
+class Service::DataParser::EventsParser
+
+  def initialize(events)
+    @events = events
+  end
+
+  def call
+    @events.each do |event|
+      gameweek = Gameweek.find_or_initialize_by(
+        name: event["name"],
+        number: event["id"]
+      )
+      gameweek.new_record? && gameweek.update(
+        deadline_time: event["deadline_time"],
+        deadline_time_epoch: event["deadline_time_epoch"]
+      )
+    end
+  end
+
+end
